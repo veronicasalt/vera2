ac9654d041d5f8613051a3fc112d0afaf484d9dd
veronicasalt <veronica49saltanov@gmail.com>
2024-12-20 23:45:46 -0800

    first commit

---
+++ www/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+    $('.text').textlate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "bounceIn"
+        },
+        out:{
+            effect: "bounceOut"
+        },
+    })
+
+});
